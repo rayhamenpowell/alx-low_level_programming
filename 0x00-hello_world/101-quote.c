2d81fea628e23552287e81d7c4399594d0827452
rayhamenpowell <powellrayhamen@gmail.com>
2023-09-07 16:26:02 -0700

    printing with write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,12 @@
 /**
  * main - printing without printf and puts
  *
- * Reture: always (1)
+ * Return: always (1)
  *
  */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+	return(1);
 
 }
